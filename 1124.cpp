99df3aeb9f40789f2e243cb0d2758ae33995452c
lulusnack <45396092+lulusnack@users.noreply.github.com>
2019-12-13 00:15:18 +0800

    Add files via upload

---
+++ 1124.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <string>
+using namespace std; // 使用std的命名空間。
+int main()
+{
+	string input;
+	while (getline(cin, input)) // 輸入並輸出一模一樣的字串。
+	{
+		cout << input << endl;
+	}
+	return 0;
+} // 結束。
